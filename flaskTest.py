0314af207d30132fbac54155e0a0323575f01437
Kedar <kedarbe.ccna@gmail.com>
2019-07-16 15:53:19 +0530

    Adding Flask project

---
+++ flaskTest.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello , World !'
+
+@app.route('/carform', methods=['GET'])
+def form1():
+    return render_template('test1.html')
+
+@app.route('/form_submitted', methods=['POST'])
+def form2():
+    print request.form
+    print request.args
+    return "Car I Selected is %s" %(request.form.get('cars'))
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='TestFlaskProject',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='kedarkulkarni',
+    author_email='',
+    description='TestFlaskProject',
+    install_requires=requirements
+)
